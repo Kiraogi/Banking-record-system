e417b3d55fc8d4bcb8c5417627eeb897db51da7a
Kiraogi <96054333+Kiraogi@users.noreply.github.com>
2023-04-08 22:05:07 +0300

    Update banking record system.cpp

--- banking record system.cpp
+++ banking record system.cpp
@@ -1,13 +1,8 @@
 #include<iostream>
 #include<fstream>
 #include<cstdlib>
-using std::cout;
-using std::cin;
-using std::endl;
-using std::fstream;
-using std::ofstream;
-using std::ifstream;
-using std::ios;
+using namespace std;
+
 class account_query
 {
 private:
